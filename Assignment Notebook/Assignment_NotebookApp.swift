6fe791ab464fd9a03d6d5acb0eac9e3cbfced9bf
Aishah <aishah122005@gmail.com>
2024-02-12 16:56:57 -0600

    Assignment Notebook

---
+++ Assignment Notebook/Assignment_NotebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_NotebookApp.swift
+//  Assignment Notebook
+//
+//  Created by Aishah Siraj on 2/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_NotebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
